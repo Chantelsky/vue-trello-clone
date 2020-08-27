13ea9fd3caeb3b4bf54f3807ead53457151cd233
Chantelsky <chantel.sky@outlook.com>
2020-08-27 19:30:03 +1000

    refactoring drag and drop

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,56 +1,67 @@
 <template>
-  <div
-    class="column"
-    draggable
-    @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
-    @dragover.prevent
-    @dragenter.prevent
-    @dragstart.self="pickupColumn($event, columnIndex)"
+  <AppDrop
+    @drop="moveTaskOrColumn"
   >
-    <div class="flex items-center mb-2 font-bold">{{ column.name }}</div>
-    <div class="list-reset">
-      <ColumnTask 
-        v-for="(task, $taskIndex) of column.tasks"
-        :key="$taskIndex"
-        :task="task"
-        :taskIndex="$taskIndex"
-        :columnIndex="columnIndex"
-        :column="column"
-        :board="board"
-      />
+    <AppDrag
+      class="column"
+      :transferData="{
+        type: 'column',
+        fromColumnIndex: columnIndex
+      }"
+    >
+      <div class="flex items-center mb-2 font-bold">
+        {{ column.name }}
+      </div>
+      <div class="list-reset">
+        <ColumnTask
+          v-for="(task, $taskIndex) of column.tasks"
+          :key="$taskIndex"
+          :task="task"
+          :taskIndex="$taskIndex"
+          :column="column"
+          :columnIndex="columnIndex"
+          :board="board"
+        />
 
-      <input
-        type="text"
-        class="block p-2 w-full bg-transparent"
-        placeholder=" + Enter new task"
-        @keyup.enter="createTask($event, column.tasks)"
-      />
-    </div>
-  </div>
+        <input
+          type="text"
+          class="block p-2 w-full bg-transparent"
+          placeholder="+ Enter new task"
+          @keyup.enter="createTask($event, column.tasks)"
+        />
+      </div>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import ColumnTask from './ColumnTask'
+import AppDrag from './AppDrag'
+import AppDrop from './AppDrop'
 import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
 
 export default {
-  components: {ColumnTask},
+  components: {
+    ColumnTask,
+    AppDrag,
+    AppDrop
+  },
   mixins: [movingTasksAndColumnsMixin],
   methods: {
-    pickupColumn(e, fromColumnIndex) {
+    pickupColumn (e, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
 
       e.dataTransfer.setData('from-column-index', fromColumnIndex)
       e.dataTransfer.setData('type', 'column')
     },
-    createTask(e, tasks) {
+    createTask (e, tasks) {
       this.$store.commit('CREATE_TASK', {
         tasks,
-        name: e.target.value,
+        name: e.target.value
       })
       e.target.value = ''
-    },
+    }
   }
 }
 </script>
--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,25 +1,33 @@
 <template>
-    <div
-        class="task"
-        draggable
-        @dragstart="pickupTask($event, taskIndex, columnIndex)"
+<AppDrop
+    @drop="moveTaskOrColumn"
+>
+<AppDrag
+class="task"
+:transferData="{
+    type: 'task',
+    fromColumnIndex: columnIndex,
+    fromTaskIndex: taskIndex
+}"
         @click="goToTask(task)"
-        @dragover.prevent
-        @dragenter.prevent
-        @drop.stop="
-            moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)
-        "
-        >
+>
         <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
         <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">{{ task.description }}</p>
-    </div>
+</AppDrag>
+</AppDrop>
 </template>
 
 <script>
   import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
+  import AppDrag from './AppDrag'
+  import AppDrop from './AppDrop'
 
     export default {
     mixins:[movingTasksAndColumnsMixin],
+    components: {
+        AppDrag,
+        AppDrop
+    },
     props: {
         task: {
             type: Object,
@@ -31,14 +39,6 @@
         }
     },
     methods: {
-      pickupTask(e, taskIndex, fromColumnIndex) {
-        e.dataTransfer.effectAllowed = 'move'
-        e.dataTransfer.dropEffect = 'move'
-
-        e.dataTransfer.setData('task-index', taskIndex)
-        e.dataTransfer.setData('from-column-index', fromColumnIndex)
-        e.dataTransfer.setData('type', 'task')
-      },
       goToTask(task) {
           this.$router.push({ name: 'task', params: { id: task.id } })
       },
